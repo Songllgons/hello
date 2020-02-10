f9034e2e7bc701423c94e856bef092ab4c760c70
Songllgons <songllgnos@foxmail.com>
2020-02-11 00:36:41 +0800

    提交一次

---
+++ src/components/hello.vue
@@ -0,0 +1,755 @@
+<template>
+  <div class="root">
+
+    <div class="top_box">
+      <div class="top_inner">
+        <div class="left">
+          <a href="#">
+            <img src="../assets/images/157199320847433454.png" alt="">
+          </a>
+        </div>
+        <div class="center">
+          <a href="#">
+            <img src="../assets/images/158062586538187674.gif" alt="">
+          </a>
+        </div>
+        <div class="right">
+          <a href="#">
+            <img src="../assets/images/157199321817918653.png" alt="">
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <!--轮播图-->
+    <div class="lunbotu_box">
+      <div class="lunbotu">
+        <img src="../assets/images/158096167189301945.jpg" alt="">
+      </div>
+    </div>
+    <!--去除bug-->
+    <div style="width: 750px;height: 60px;"></div>
+
+
+    <!--限时抢购-->
+    <div class="xianshiqianggou_box">
+      <a href="#" class="xianshiqianggou">
+        <img src="../assets/images/157796934543366156.webp" alt="">
+        <span>限时抢购</span>
+      </a>
+      <a href="#" class="xianshiqianggou">
+        <img src="../assets/images/157796934543366156.webp" alt="">
+        <span>限时抢购</span>
+      </a>
+      <a href="#" class="xianshiqianggou">
+        <img src="../assets/images/157796934543366156.webp" alt="">
+        <span>限时抢购</span>
+      </a>
+      <a href="#" class="xianshiqianggou">
+        <img src="../assets/images/157796934543366156.webp" alt="">
+        <span>限时抢购</span>
+      </a>
+      <a href="#" class="xianshiqianggou">
+        <img src="../assets/images/157796934543366156.webp" alt="">
+        <span>限时抢购</span>
+      </a>
+      <a href="#" class="xianshiqianggou">
+        <img src="../assets/images/157796934543366156.webp" alt="">
+        <span>限时抢购</span>
+      </a>
+      <a href="#" class="xianshiqianggou">
+        <img src="../assets/images/157796934543366156.webp" alt="">
+        <span>限时抢购</span>
+      </a>
+      <a href="#" class="xianshiqianggou">
+        <img src="../assets/images/157796934543366156.webp" alt="">
+        <span>限时抢购</span>
+      </a>
+      <a href="#" class="xianshiqianggou">
+        <img src="../assets/images/157796934543366156.webp" alt="">
+        <span>限时抢购</span>
+      </a>
+      <a href="#" class="xianshiqianggou">
+        <img src="../assets/images/157796934543366156.webp" alt="">
+        <span>限时抢购</span>
+      </a>
+    </div>
+    <!--科学防护-->
+    <div class="kexue_box">
+      <a href="#" class="kexue_inner">
+        <img src="../assets/images/158090159174996859.png" alt="">
+      </a>
+      <a href="#" class="kexue_inner">
+        <img src="../assets/images/158090160350449341.png" alt="">
+      </a>
+    </div>
+    <!--老人在家-->
+    <div class="laoren_box">
+      <a href="#" class="laoren_inner">
+        <img src="../assets/images/158104987382134277.png" alt="">
+      </a>
+    </div>
+    <!--新人大礼包-->
+    <div class="xinren_box">
+      <a href="#" class="xinren_inner">
+        <img src="../assets/images/157330223943814123.png" alt="">
+      </a>
+      <a href="#" class="xinren_inner">
+        <img src="../assets/images/157330224785306243.gif" alt="">
+      </a>
+      <a href="#" class="xinren_inner">
+        <img src="../assets/images/157330227372982455.gif" alt="">
+      </a>
+    </div>
+
+    <!--天天低价-->
+    <div class="tiantain_box">
+      <div class="tiantian_inner">
+        <div class="title">
+          <span>天天低价</span>
+          <!--          <span>倒计时</span>-->
+        </div>
+        <p>人气好货限时抢</p>
+
+        <div class="desc_box">
+          <a href="#" class="desc">
+            <img src="../assets/images/100443353615426025picH_1.jpg" alt="">
+            <div class="price">
+              <span>￥</span>
+              <span>4</span>
+              <span>.9</span>
+            </div>
+          </a>
+          <a href="#" class="desc">
+            <img src="../assets/images/10044338211556835651picH_1.jpg" alt="">
+            <div class="price">
+              <span>￥</span>
+              <span>4</span>
+              <span>.9</span>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="tiantian_inner">
+        <div class="title">
+          <span>苏宁秒杀</span>
+          <!--          <span>倒计时</span>-->
+        </div>
+        <p>品质好货天天秒</p>
+
+        <div class="desc_box">
+          <a href="#" class="desc">
+            <img src="../assets/images/157967604788407850.webp" alt="">
+            <div class="price">
+              <span>￥</span>
+              <span>4</span>
+              <span>.9</span>
+            </div>
+          </a>
+          <a href="#" class="desc">
+            <img src="../assets/images/157967603163225572.webp" alt="">
+            <div class="price">
+              <span>￥</span>
+              <span>4</span>
+              <span>.9</span>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="tiantian_inner">
+        <div class="title">
+          <span>百亿补贴</span>
+          <!--          <span>倒计时</span>-->
+        </div>
+        <p>抢超值好货</p>
+
+        <div class="desc_box">
+          <a href="#" class="desc">
+            <img src="../assets/images/158100080889831535.webp" alt="">
+            <div class="price">
+              <span>￥</span>
+              <span>4</span>
+              <span>.9</span>
+            </div>
+          </a>
+          <a href="#" class="desc">
+            <img src="../assets/images/158100075923586371.webp" alt="">
+            <div class="price">
+              <span>￥</span>
+              <span>4</span>
+              <span>.9</span>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="tiantian_inner">
+        <div class="title">
+          <span>苏宁拼购</span>
+          <!--          <span>倒计时</span>-->
+        </div>
+        <p>2人拼更实惠 全场包邮</p>
+
+        <div class="desc_box">
+          <a href="#" class="desc">
+            <img src="../assets/images/yeY1OVjuS7CL2OVtsGXPfg.jpg" alt="">
+            <div class="price">
+              <span>￥</span>
+              <span>4</span>
+              <span>.9</span>
+            </div>
+          </a>
+          <a href="#" class="desc">
+            <img src="../assets/images/983300509156675552949890_x.jpg" alt="">
+            <div class="price">
+              <span>￥</span>
+              <span>4</span>
+              <span>.9</span>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+    <!--为您推荐-->
+    <div class="tuijian_box">
+      <a href="#" class="tuijian_left">
+        <p>为您推荐</p>
+        <p>精选频道</p>
+      </a>
+      <div class="tuijian_right">
+        <a href="#" class="tuijian_inner">
+          <img src="../assets/images/0070221027000000011597144893.webp" alt="">
+          <span>苏宁国际</span>
+        </a>
+        <a href="#" class="tuijian_inner">
+          <img src="../assets/images/0070221027000000011597144893.webp" alt="">
+          <span>苏宁国际</span>
+        </a>
+        <a href="#" class="tuijian_inner">
+          <img src="../assets/images/0070221027000000011597144893.webp" alt="">
+          <span>苏宁国际</span>
+        </a>
+        <a href="#" class="tuijian_inner">
+          <img src="../assets/images/0070221027000000011597144893.webp" alt="">
+          <span>苏宁国际</span>
+        </a>
+      </div>
+
+    </div>
+    <!--苏宁头条-->
+    <div class="news_box">
+      <a href="#" class="news_left">
+        <img src="../assets/images/157164888626937126.png" alt="">
+      </a>
+      <div class="news_right">
+        <span>热门</span>
+        <span>华硕灵耀S2代笔记本，颜值闪耀C位出道</span>
+
+      </div>
+    </div>
+    <!--超市补给站-->
+    <div class="chaoshi_box">
+      <a href="#" class="chaoshi">
+        <img src="../assets/images/158098358492712291.png" alt="">
+      </a>
+    </div>
+    <!--居家健身-->
+    <div class="jujai_box">
+      <div class="jujai_wrap">
+        <div class="jujai_inner">
+          <a href="#">
+            <img src="../assets/images/158098335594883382.png" alt="">
+          </a>
+          <a href="#">
+            <img src="../assets/images/158098336829035546.png" alt="">
+          </a>
+          <a href="#">
+            <img src="../assets/images/158098335594883382.png" alt="">
+          </a>
+          <a href="#">
+            <img src="../assets/images/158098336829035546.png" alt="">
+          </a>
+        </div>
+        <div></div>
+
+        <div class="jujai_inner_01">
+          <a href="#" class="jujai_inner_01_inner">
+            <img src="../assets/images/156937734386141881.webp" alt="">
+            <p>生活家电</p>
+            <span>满200减20</span>
+          </a>
+          <a href="#" class="jujai_inner_01_inner">
+            <img src="../assets/images/156937734386141881.webp" alt="">
+            <p>生活家电</p>
+            <span>满200减20</span>
+          </a>
+          <a href="#" class="jujai_inner_01_inner">
+            <img src="../assets/images/156937734386141881.webp" alt="">
+            <p>生活家电</p>
+            <span>满200减20</span>
+          </a>
+          <a href="#" class="jujai_inner_01_inner">
+            <img src="../assets/images/156937734386141881.webp" alt="">
+            <p>生活家电</p>
+            <span>满200减20</span>
+          </a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "hello"
+  }
+</script>
+<style scoped lang="less">
+  @import "../assets/css/normalize.css";
+
+  /* * {
+     margin: 0;
+     padding: 0;
+     box-sizing: border-box;
+   }*/
+
+  a {
+    text-decoration: none;
+  }
+
+  .top_box {
+    width: 750px;
+    height: 88px;
+    line-height: 88px;
+    border: 1px solid #000;
+
+    background-color: red;
+
+    .top_inner {
+      height: 60px;
+      /*border: 1px solid saddlebrown;*/
+      margin-top: 12px;
+      display: flex;
+      justify-content: space-between;
+
+
+      /*左*/
+
+      .left {
+        width: 36px;
+        /*border: 1px solid violet;*/
+        padding-left: 34px;
+
+        img {
+          width: 100%;
+          height: 100%;
+        }
+      }
+
+      /*中*/
+
+      .center {
+        /*border: 1px solid chartreuse;*/
+        width: 450px;
+        /*height: 55px;*/
+
+        img {
+          width: 100%;
+          height: 100%;
+        }
+
+      }
+
+      /*右*/
+
+      .right {
+        /*border: 1px solid saddlebrown;*/
+        width: 36px;
+        /*height: 60px;*/
+        padding-right: 34px;
+
+        img {
+          width: 100%;
+          height: 100%;
+        }
+
+      }
+    }
+
+
+  }
+
+  /*轮播图*/
+  .lunbotu_box {
+    /*border: 1px solid #000;*/
+    width: 750px;
+    height: 200px;
+
+    .lunbotu {
+      width: 750px;
+      height: 260px;
+      border: 1px solid aqua;
+
+      img {
+        width: 702px;
+        height: 100%;
+      }
+    }
+  }
+
+  /*限时抢购*/
+  .xianshiqianggou_box {
+    width: 750px;
+    height: 308px;
+    /*padding: 0 33px;*/
+    display: flex;
+    flex-wrap: wrap;
+
+
+    .xianshiqianggou {
+      /*border: 1px solid #000;*/
+      display: block;
+      width: 150px;
+      height: 142px;
+      text-align: center;
+
+      img {
+        width: 84px;
+        height: 84px;
+        margin-top: 20px;
+        /*display: block;*/
+      }
+
+      span {
+        display: block;
+        font-size: 22px;
+        color: #666666;
+
+      }
+    }
+  }
+
+  /*科学防护*/
+  .kexue_box {
+    width: 750px;
+    /*border: 1px solid #000;*/
+    height: 210px;
+    display: flex;
+
+    .kexue_inner {
+      display: block;
+      /*border: 1px solid #000;*/
+
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+
+  /*老人在家*/
+  .laoren_box {
+    width: 750px;
+    height: 70px;
+
+    .laoren_inner {
+      display: block;
+
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+
+  /*新人大礼包*/
+  .xinren_box {
+    width: 750px;
+    height: 234px;
+    display: flex;
+    margin-top: 12px;
+
+    .xinren_inner {
+      display: block;
+      height: 234px;
+
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+
+    &:nth-of-type(1) {
+      width: 374px;
+    }
+
+    &:nth-of-type(2) {
+      width: 186px;
+    }
+
+    &:nth-of-type(3) {
+      width: 186px;
+    }
+
+  }
+
+  /*天天低价*/
+  .tiantain_box {
+    width: 750px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    margin-top: 15px;
+
+    .tiantian_inner {
+      /*border: 1px solid #000;*/
+      width: 350px;
+
+      p {
+        margin: 0;
+        margin-top: 5px;
+        color: #999999;
+        font-size: 24px;
+        padding-left: 20px;
+
+      }
+
+      .title {
+        padding-left: 20px;
+
+        span {
+          font-size: 28px;
+          color: #333333;
+          font-weight: 700;
+        }
+
+      }
+
+      .desc_box {
+        display: flex;
+        justify-content: space-around;
+
+        .desc {
+          display: block;
+
+          img {
+            width: 120px;
+            height: 120px;
+            /*float: left;*/
+
+          }
+
+          .price {
+            width: 100%;
+            text-align: center;
+
+          }
+        }
+      }
+
+    }
+
+  }
+
+  /*为您推荐*/
+  .tuijian_box {
+    width: 750px;
+    height: 172px;
+    display: flex;
+    flex-wrap: wrap;
+    align-content: center;
+    /*border: 1px solid #000;*/
+    padding-left: 20px;
+
+
+    .tuijian_left {
+      width: 173px;
+      /*padding-left: 35px;*/
+
+      > p {
+        margin: 0;
+      }
+
+      > p:nth-of-type(1) {
+        font-size: 32px;
+        color: #444444;
+        font-weight: 700;
+
+      }
+
+      > p:nth-of-type(2) {
+        font-size: 24px;
+        color: #999999;
+        font-weight: 700;
+      }
+
+    }
+
+    .tuijian_right {
+      width: 508px;
+      display: flex;
+      flex-wrap: wrap;
+
+      justify-content: space-between;
+
+      .tuijian_inner {
+        width: 120px;
+
+        img {
+          width: 100px;
+          height: 100px;
+          display: block;
+          margin: 0 auto;
+        }
+
+        span {
+          width: 100%;
+          height: 30px;
+          display: block;
+          text-align: center;
+          font-size: 22px;
+          color: #333333;
+
+        }
+      }
+    }
+
+
+  }
+
+  /*苏宁头条*/
+  .news_box {
+    width: 702px;
+    height: 66px;
+    display: flex;
+
+    .news_left {
+      display: block;
+      width: 112px;
+      line-height: 66px;
+      /*border: 1px solid violet;*/
+
+
+      img {
+        height: 31px;
+        width: 100%;
+        vertical-align: middle;
+      }
+    }
+
+    .news_right {
+      line-height: 66px;
+      width: 520px;
+      padding-left: 50px;
+
+      > span:nth-of-type(1) {
+        background: #fc0;
+        color: #222222;
+        font-size: 18px;
+        padding: 0 10px;
+        -webkit-border-radius: 20px;
+        -moz-border-radius: 20px;
+        border-radius: 20px;
+      }
+
+      > span:nth-of-type(2) {
+        color: #333333;
+        font-size: 24px;
+        padding-left: 20px;
+        text-wrap: none;
+        overflow: auto;
+        text-overflow: ellipsis;
+      }
+
+    }
+  }
+
+  /*超市补给站*/
+  .chaoshi_box {
+    width: 750px;
+    height: 210px;
+
+    .chaoshi {
+      display: block;
+      font-size: 0;
+      height: 100%;
+
+      img {
+        width: 100%;
+        height: 100%;
+        display: block;
+      }
+    }
+  }
+
+  /*居家健身*/
+  .jujai_box {
+    width: 750px;
+
+    .jujai_wrap {
+
+      .jujai_inner {
+        display: flex;
+        flex-wrap: wrap;
+
+
+        a {
+          display: block;
+          font-size: 0;
+
+          img {
+            width: 375px;
+            height: 150px;
+          }
+        }
+      }
+
+      .jujai_inner_01 {
+        background: url("../assets/images/158099183142418502.jpg");
+        padding: 0 22px;
+        padding-top: 10px;
+        display: flex;
+        justify-content: space-between;
+
+        .jujai_inner_01_inner {
+          width: 167px;
+          height: 243px;
+          background: url("../assets/images/156937734114433686.png");
+          display: block;
+          text-align: center;
+
+          img {
+            width: 140px;
+            height: 140px;
+            vertical-align: middle;
+
+          }
+          p{
+            color: #fffff0;
+            font-size: 26px;
+            margin: 0;
+            padding-top: 15px;
+
+          }
+          span{
+            display: block;
+            width: 140px;
+            margin: 0 auto;
+            margin-top: 15px;
+            height: 32px;
+            line-height: 32px;
+            -webkit-border-radius: 16px;
+            -moz-border-radius: 16px;
+            border-radius: 16px;
+            background-color: #fff;
+            color:#E244AA;
+            font-size: 20px;
+          }
+        }
+      }
+    }
+  }
+
+
+</style>
